7dd2994cc193327647ac09f871941101dcc1a9dd
Lennart Regebro <regebro@gmail.com>
2023-05-01 20:29:27 +0200

    Github actions

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -18,71 +18,89 @@ def test_make_matrix():
 
     # Skew
     m3 = transform.make_matrix(ax=2, ay=5)
-    assert m3.tolist() == [
-        [1, -2.185039863261519, 0],
-        [-3.380515006246586, 1, 0],
-        [0, 0, 1],
-    ]
+    numpy.testing.assert_array_almost_equal(
+        m3,
+        [
+            [1, -2.185039863261519, 0],
+            [-3.380515006246586, 1, 0],
+            [0, 0, 1],
+        ],
+    )
 
     # Translate + Scale
     m4 = transform.make_matrix(tx=8.6, ty=7, sx=5, sy=2.1)
     assert m4.tolist() == [[5, 0, 8.6], [0, 2.1, 7], [0, 0, 1]]
 
     # Translate and scale should be the same as translate @ scale
-    assert m4.tolist() == (m1 @ m2).tolist()
+    numpy.testing.assert_array_almost_equal(m4, (m1 @ m2))
 
     # Translate + Skew
     m4 = transform.make_matrix(tx=8.6, ty=7, ax=2, ay=5)
-    assert m4.tolist() == [
-        [1, -2.185039863261519, 8.6],
-        [-3.380515006246586, 1, 7],
-        [0, 0, 1],
-    ]
+    numpy.testing.assert_array_almost_equal(
+        m4.tolist(),
+        [
+            [1, -2.185039863261519, 8.6],
+            [-3.380515006246586, 1, 7],
+            [0, 0, 1],
+        ],
+    )
 
     # Translate and skew should be the same as translate @ skew
     assert m4.tolist() == (m1 @ m3).tolist()
 
     # Scale + Skew
     m4 = transform.make_matrix(sx=5, sy=2.1, ax=2, ay=5)
-    assert m4.tolist() == [
-        [5, -10.925199316307594, 0],
-        [-7.099081513117831, 2.1, 0],
-        [0, 0, 1],
-    ]
+    numpy.testing.assert_array_almost_equal(
+        m4,
+        [
+            [5, -10.925199316307594, 0],
+            [-7.099081513117831, 2.1, 0],
+            [0, 0, 1],
+        ],
+    )
 
     # Scale and skew should be the same as scale @ skew
-    assert m4.tolist() == (m2 @ m3).tolist()
+    numpy.testing.assert_array_almost_equal(m4, (m2 @ m3))
 
     # Translate + Scale + Skew
     m4 = transform.make_matrix(tx=8.6, ty=7, sx=5, sy=2.1, ax=2, ay=5)
-    assert m4.tolist() == [
-        [5, -10.925199316307594, 8.6],
-        [-7.099081513117831, 2.1, 7],
-        [0, 0, 1],
-    ]
+    numpy.testing.assert_array_almost_equal(
+        m4,
+        [
+            [5, -10.925199316307594, 8.6],
+            [-7.099081513117831, 2.1, 7],
+            [0, 0, 1],
+        ],
+    )
 
     # Translate and Scale and skew should be the same as translate @ scale @ skew
-    assert m4.tolist() == (m1 @ m2 @ m3).tolist()
+    numpy.testing.assert_array_almost_equal(m4, (m1 @ m2 @ m3))
 
     # Rotate
     m4 = transform.make_matrix(r=1)
     # cos(1) == 0.5403023058681398
     # sin(1) == 0.8414709848078965
-    assert m4.tolist() == [
-        [0.5403023058681398, -0.8414709848078965, 0],
-        [0.8414709848078965, 0.5403023058681398, 0],
-        [0, 0, 1],
-    ]
+    numpy.testing.assert_array_almost_equal(
+        m4,
+        [
+            [0.5403023058681398, -0.8414709848078965, 0],
+            [0.8414709848078965, 0.5403023058681398, 0],
+            [0, 0, 1],
+        ],
+    )
 
     # Rotate with offset
     m4 = transform.make_matrix(r=1, cx=10, cy=4)
     # cos(1) == 0.5403023058681398
     # sin(1) == 0.8414709848078965
-    assert m4.tolist() == [
-        [0.5403023058681398, -0.8414709848078965, 7.962860880550188],
-        [0.8414709848078965, 0.5403023058681398, -6.5759190715515246],
-        [0, 0, 1],
-    ]
+    numpy.testing.assert_array_almost_equal(
+        m4,
+        [
+            [0.5403023058681398, -0.8414709848078965, 7.962860880550188],
+            [0.8414709848078965, 0.5403023058681398, -6.5759190715515246],
+            [0, 0, 1],
+        ],
+    )
 
     # And that's the same as applying a translate, rotating and deducting the translation:
     m5 = transform.make_matrix(tx=10, ty=4)
@@ -92,11 +110,14 @@ def test_make_matrix():
 
     # Do them all!
     m5 = transform.make_matrix(tx=8.6, ty=7, sx=5, sy=2.1, ax=2, ay=5, r=1, cx=10, cy=4)
-    assert m5.tolist() == [
-        [-6.49172669857521, -10.1102653067095, 120.25753094735971],
-        [-2.0685610429868637, 7.108305954397887, -63.33842851890122],
-        [0, 0, 1],
-    ]
+    numpy.testing.assert_array_almost_equal(
+        m5,
+        [
+            [-6.49172669857521, -10.1102653067095, 120.25753094735971],
+            [-2.0685610429868637, 7.108305954397887, -63.33842851890122],
+            [0, 0, 1],
+        ],
+    )
     numpy.testing.assert_array_almost_equal(m5, m1 @ m2 @ m3 @ m4)
 
 
@@ -145,21 +166,31 @@ def test_parsing():
     assert m.tolist() == [[2, 0, 0], [0, 2, 0], [0, 0, 1]]
 
     m = transform.parse("rotate(45)")
-    assert m.tolist() == [
-        [0.5253219888177297, -0.8509035245341184, 0],
-        [0.8509035245341184, 0.5253219888177297, 0],
-        [0, 0, 1],
-    ]
+    numpy.testing.assert_array_almost_equal(
+        m,
+        [
+            [0.5253219888177297, -0.8509035245341184, 0],
+            [0.8509035245341184, 0.5253219888177297, 0],
+            [0, 0, 1],
+        ],
+    )
 
     m = transform.parse("skewX(5)")
-    assert m.tolist() == [[1, -3.380515006246586, 0], [0, 1, 0], [0, 0, 1]]
+    numpy.testing.assert_array_almost_equal(
+        m, [[1, -3.380515006246586, 0], [0, 1, 0], [0, 0, 1]]
+    )
 
     m = transform.parse("skewY(5)")
-    assert m.tolist() == [[1, 0, 0], [-3.380515006246586, 1, 0], [0, 0, 1]]
+    numpy.testing.assert_array_almost_equal(
+        m, [[1, 0, 0], [-3.380515006246586, 1, 0], [0, 0, 1]]
+    )
 
     m = transform.parse("translate(-10 -20) scale(2) rotate(45)translate(5,10)")
-    assert m.tolist() == [
-        [1.0506439776354595, -1.7018070490682369, -21.76485060250507],
-        [1.7018070490682369, 1.0506439776354595, -0.984524978304222],
-        [0, 0, 1],
-    ]
+    numpy.testing.assert_array_almost_equal(
+        m,
+        [
+            [1.0506439776354595, -1.7018070490682369, -21.76485060250507],
+            [1.7018070490682369, 1.0506439776354595, -0.984524978304222],
+            [0, 0, 1],
+        ],
+    )
